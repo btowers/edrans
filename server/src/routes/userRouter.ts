1536bf3d32eb1114f09fdf1d99d85bf80691c802
brako2 <49849533+brako2@users.noreply.github.com>
2022-01-31 01:44:43 -0300

    auth: facebook

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 // Public Routes
 router.post('/signup', userC.signup)
 router.post('/login', userC.login)
-router.get('/facebook', userC.facebookLogin)
+router.get('/facebook', passport.authenticate('facebook', { session: false, scope: ['email'] }))
 router.get('/facebook/redirect', userC.facebookLogin)
 
 export default router
