d5886e624ea1aece79265ec91b0e47ba726923ae
brako2 <49849533+brako2@users.noreply.github.com>
2022-01-30 23:02:13 -0300

    images: bluebird

--- server/src/services/image.ts
+++ server/src/services/image.ts
@@ -1,7 +1,7 @@
-import fs from 'fs'
+// import fs from 'fs'
 import config from '../config'
 import aws from 'aws-sdk'
-import bluebird from 'bluebird'
+// import bluebird from 'bluebird'
 
 class Image {
   s3 = new aws.S3({
@@ -10,25 +10,25 @@ class Image {
     region: config.AWS_REGION,
   })
 
-  async saveImage(file: { path: string; filename: string }): Promise<string> {
-    aws.config.setPromisesDependency(bluebird)
+  // async saveImage(file: { path: string; filename: string }): Promise<string> {
+  //   aws.config.setPromisesDependency(bluebird)
 
-    const params = {
-      ACL: 'public-read',
-      Bucket: config.AWS_S3_BUCKET_NAME,
-      Body: fs.createReadStream(file.path),
-      Key: `${file.filename}`,
-    }
-    return new Promise((resolve, reject) => {
-      this.s3.upload(params, (err: any, data: any) => {
-        if (err) {
-          reject(err)
-        }
-        fs.unlinkSync(file.path)
-        resolve(data.key)
-      })
-    })
-  }
+  //   const params = {
+  //     ACL: 'public-read',
+  //     Bucket: config.AWS_S3_BUCKET_NAME,
+  //     Body: fs.createReadStream(file.path),
+  //     Key: `${file.filename}`,
+  //   }
+  //   return new Promise((resolve, reject) => {
+  //     this.s3.upload(params, (err: any, data: any) => {
+  //       if (err) {
+  //         reject(err)
+  //       }
+  //       fs.unlinkSync(file.path)
+  //       resolve(data.key)
+  //     })
+  //   })
+  // }
 
   async deleteImages(filenames: Array<string>): Promise<aws.S3.DeleteObjectOutput> {
     const params = {
