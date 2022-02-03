8c21018e593bc29c50ee8cad0301eb5c7dddc6eb
brako2 <49849533+brako2@users.noreply.github.com>
2022-02-02 21:28:24 -0300

    vue: folder structure

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -6,70 +6,75 @@ const routes = [
       {
         path: "",
         name: "Home",
-        component: () => import("src/pages/products/ProductList.vue"),
+        component: () => import("pages/Home.vue"),
         meta: {
           requiresAuth: false,
         },
-      },
+        children: [
+          {
+            path: "",
+            name: "ProductList",
+            component: () => import("components/products/ProductList.vue"),
+            meta: {
+              requiresAuth: false,
+            },
+          },
 
-      {
-        path: "cart",
-        name: "CartPage",
-        component: () => import("src/pages/Cart.vue"),
-        meta: {
-          requiresAuth: true,
-        },
-      },
-      {
-        path: "profile",
-        name: "ProfilePage",
-        component: () => import("src/pages/UserProfile.vue"),
-        meta: {
-          requiresAuth: true,
-        },
-      },
-    ],
-  },
-  {
-    path: "/product",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
-        path: ":id",
-        name: "ProductDetailPage",
-        component: () => import("src/pages/products/ProductDetail.vue"),
-        meta: {
-          requiresAuth: false,
-        },
-      },
-      {
-        path: "new",
-        name: "ProductNewPage",
-        component: () => import("src/pages/products/ProductNew.vue"),
-        meta: {
-          requiresAuth: true,
-          requiresAdmin: true,
-        },
-      },
-      {
-        path: "edit/:id",
-        name: "ProductEditPage",
-        component: () => import("src/pages/products/ProductEdit.vue"),
-        meta: {
-          requiresAuth: true,
-          requiresAdmin: true,
-        },
+          {
+            path: "cart",
+            name: "CartPage",
+            component: () => import("components/Cart.vue"),
+            meta: {
+              requiresAuth: true,
+            },
+          },
+          {
+            path: "profile",
+            name: "ProfilePage",
+            component: () => import("components/Profile.vue"),
+            meta: {
+              requiresAuth: true,
+            },
+          },
+          {
+            path: "product/:id",
+            name: "ProductDetail",
+            component: () => import("components/products/ProductDetail.vue"),
+            meta: {
+              requiresAuth: false,
+            },
+          },
+          {
+            path: "product/new",
+            name: "ProductNew",
+            component: () => import("components/products/ProductNew.vue"),
+            meta: {
+              requiresAuth: true,
+              requiresAdmin: true,
+            },
+          },
+          {
+            path: "product/edit/:id",
+            name: "ProductEdit",
+            component: () => import("components/products/ProductEdit.vue"),
+            meta: {
+              requiresAuth: true,
+              requiresAdmin: true,
+            },
+          },
+        ],
       },
     ],
   },
+
   {
     path: "/login",
-    component: () => import("src/layouts/CleanLayout.vue"),
+    component: () => import("layouts/CleanLayout.vue"),
     children: [
       {
         path: "",
         name: "LoginPage",
-        component: () => import("src/pages/Login.vue"),
+        component: () => import("pages/Login.vue"),
         meta: {
           requiresAuth: false,
         },
@@ -78,12 +83,12 @@ const routes = [
   },
   {
     path: "/register",
-    component: () => import("src/layouts/CleanLayout.vue"),
+    component: () => import("layouts/CleanLayout.vue"),
     children: [
       {
         path: "",
         name: "RegisterPage",
-        component: () => import("src/pages/Register.vue"),
+        component: () => import("pages/Register.vue"),
         meta: {
           requiresAuth: false,
         },
